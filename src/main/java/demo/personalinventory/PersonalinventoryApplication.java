376488ae713d93b5ed8626170cc99178514c56a3
Szalay Tamás <szalaytamas90@gmail.com>
2022-11-06 16:31:16 +0100

    missing parts

---
+++ src/main/java/demo/personalinventory/PersonalinventoryApplication.java
@@ -0,0 +1,13 @@
+package demo.personalinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalinventoryApplication.class, args);
+	}
+
+}
