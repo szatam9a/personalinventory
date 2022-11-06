2a75102c93e0913d1f74008b3f617b3365ec2ddd
Szalay Tamás <szalaytamas90@gmail.com>
2022-11-06 13:33:16 +0100

    Address entity

---
+++ src/main/java/demo/personalinventory/address/Address.java
@@ -0,0 +1,25 @@
+package demo.personalinventory.address;
+
+import demo.personalinventory.contact.Contact;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Long id;
+    @Column(name = "address_city")
+    private String city;
+    @Column(name = "address_street")
+    private String street;
+    @OneToMany(mappedBy = "address")
+    private List<Contact> contactList;
+}
