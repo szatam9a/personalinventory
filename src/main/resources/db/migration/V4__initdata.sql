5bb1f3726f62296f57fbcd58fe0a160a908f3ccd
Szalay Tamás <szalaytamas90@gmail.com>
2022-11-06 18:01:50 +0100

    flyway migration file

--- src/main/resources/db/migration/V4__initdata.sql
+++ src/main/resources/db/migration/V4__initdata.sql
@@ -1,7 +1,7 @@
-INSERT INTO `crud2`.`addresses` (`address_id`, `address_city`, `address_street`) VALUES ('1', 'Pest', 'Szondy');
-INSERT INTO `crud2`.`addresses` (`address_id`, `address_city`, `address_street`) VALUES ('2', 'APest', 'Szondy');
-INSERT INTO `crud2`.`addresses` (`address_id`, `address_city`, `address_street`) VALUES ('3', 'CPest', 'Szondy');
-INSERT INTO `crud2`.`addresses` (`address_id`, `address_city`, `address_street`) VALUES ('4', 'BPest', 'Szondy');
-INSERT INTO `crud2`.`contacts` (`contact_id`, `contact_address_id`) VALUES ('1', '2');
-INSERT INTO `crud2`.`contacts` (`contact_id`, `contact_address_id`) VALUES ('2', '3');
-INSERT INTO `crud2`.`contacts` (`contact_id`, `contact_address_id`) VALUES ('3', '3');
+INSERT INTO `addresses` (`address_id`, `address_city`, `address_street`) VALUES ('1', 'Pest', 'Szondy');
+INSERT INTO `addresses` (`address_id`, `address_city`, `address_street`) VALUES ('2', 'APest', 'Szondy');
+INSERT INTO `addresses` (`address_id`, `address_city`, `address_street`) VALUES ('3', 'CPest', 'Szondy');
+INSERT INTO `addresses` (`address_id`, `address_city`, `address_street`) VALUES ('4', 'BPest', 'Szondy');
+INSERT INTO `contacts` (`contact_id`, `contact_address_id`) VALUES ('1', '2');
+INSERT INTO `contacts` (`contact_id`, `contact_address_id`) VALUES ('2', '3');
+INSERT INTO ``contacts` (`contact_id`, `contact_address_id`) VALUES ('3', '3');
