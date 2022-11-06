f1938b08b82709affc151b5decd41fe7fa66040e
Szalay Tamás <szalaytamas90@gmail.com>
2022-11-06 16:52:39 +0100

    user not found exception

---
+++ src/test/java/demo/personalinventory/contact/ContactServiceTest.java
@@ -0,0 +1,12 @@
+package demo.personalinventory.contact;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactServiceTest {
+
+    @Test
+    void createContactTest() {
+    }
+}
