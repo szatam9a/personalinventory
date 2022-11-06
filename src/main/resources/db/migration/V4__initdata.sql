2de3ee8acbde5b77dc59e313d308b76e283537dc
Szalay Tamás <szalaytamas90@gmail.com>
2022-11-06 18:02:07 +0100

    flyway migration file

--- src/main/resources/db/migration/V4__initdata.sql
+++ src/main/resources/db/migration/V4__initdata.sql
@@ -4,4 +4,4 @@ INSERT INTO `addresses` (`address_id`, `address_city`, `address_street`) VALUES 
 INSERT INTO `addresses` (`address_id`, `address_city`, `address_street`) VALUES ('4', 'BPest', 'Szondy');
 INSERT INTO `contacts` (`contact_id`, `contact_address_id`) VALUES ('1', '2');
 INSERT INTO `contacts` (`contact_id`, `contact_address_id`) VALUES ('2', '3');
-INSERT INTO ``contacts` (`contact_id`, `contact_address_id`) VALUES ('3', '3');
+INSERT INTO `contacts` (`contact_id`, `contact_address_id`) VALUES ('3', '3');
